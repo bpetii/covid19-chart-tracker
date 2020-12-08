a1cdd953f709cbe31005c06fd6f8c8c35844b78f
bpetii <biro.peti09@gmail.com>
2020-12-08 15:59:10 +0100

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React, { useEffect } from "react";
+
 import styles from "./App.module.css";
 
 import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
 
+import { fetchData } from "./api";
+
 function App() {
+  useEffect(async() => {
+    
+
+    const data = await fetchData();
+    console.log(data);
+  }, []);
   return (
     <div className={styles.container}>
       <Cards />
